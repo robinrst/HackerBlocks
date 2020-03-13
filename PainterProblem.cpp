4f0d098d1aff29d79f5d49ba092730f8e65316e4
robinrst <31628501+robinrst@users.noreply.github.com>
2020-03-13 12:55:55 +0530

    Add files via upload

---
+++ PainterProblem.cpp
@@ -0,0 +1,135 @@
+#include        "algorithm"
+#include        "iostream"
+#include        "numeric"
+#include        "iomanip"
+#include        "cstring"
+#include        "math.h"
+#include        "bitset"
+#include        "string"
+#include        "vector"
+#include        "ctime"
+#include        "queue"
+#include        "stack"
+#include        "map"
+#include        "set"
+
+#include        "ext/pb_ds/assoc_container.hpp" // Common file
+#include        "ext/pb_ds/tree_policy.hpp" // Including tree_order_statistics_node_update
+#include        "ext/pb_ds/detail/standard_policies.hpp"
+
+using namespace std;
+using namespace __gnu_pbds;
+
+
+#define          f first
+#define          lgn 25
+#define          endl '\n'
+#define          sc second
+#define          N (int)2e5+5
+#define          pb push_back
+#define          mod 10000003
+#define          ld long double
+#define          vi vector<int>
+#define          eb emplace_back
+#define          vpii vector<pii>
+#define          mii map<int,int>
+#define          int long long 
+#define          pii pair<int,int>
+#define          pq priority_queue
+#define          BLOCK (int)sqrt(N)
+#define          test(x) while(x--)
+#define          all(x) begin(x),end(x)
+#define          allr(x) rbegin(x),rend(x)
+#define          fo(i,a,n) for(int i=a;i<n;i++)
+#define          rfo(i,n,a) for(int i=n;i>=a;i--)
+#define          FAST ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define          time() cerr << "Time : " << (double)clock() / (double)CLOCKS_PER_SEC << "s\n"
+#define          PI acos(-1.0)
+#define 		 bug(...)       __f (#__VA_ARGS__, __VA_ARGS__)
+
+typedef tree< int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update > 
+OS ;
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args)
+{
+     const char* comma = strchr (names + 1, ',');
+     cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+inline void INP()
+{
+	#ifndef ONLINE_JUDGE
+	    freopen("input.txt","r",stdin);   
+	    freopen("output.txt","w",stdout);
+	#endif 
+}
+
+const int inf = 0x3f3f3f3f;
+const int INF = 0x3f3f3f3f3f3f3f3f;
+
+int n,m,k,q,T;
+string s;
+vi adj[N];
+int vis[N],par[N],a[N];
+
+bool check(int mid)
+{
+
+	int now = 0;
+	int painter = 1;
+
+	fo(i,0,n)
+	{
+		now += a[i];
+
+		if( now > mid )
+		{
+			now = a[i];
+			painter++;
+		}
+	}
+	return ( painter <= k );
+}
+
+void go()
+{
+	cin >> n >> k >> T;
+
+	int sum = 0 , mx = -1;
+
+	fo(i,0,n)
+	{
+		cin >> a[i];
+		sum += a[i];
+		mx = max( mx , a[i] );
+	}
+
+	int l = mx , r = sum;
+	int ans = 0;
+
+	while( l <= r )
+	{
+		int mid = l+r>>1;
+
+		if( check(mid) ) ans = mid , r = mid - 1;
+		else l = mid + 1;
+	}	
+
+	ans = ( ans * T )%mod;
+
+	cout << ans << endl;
+}
+
+int32_t main()
+{
+	INP(); 
+	FAST;     
+	int t=1; 
+	// cin>>t;
+	test(t) go();
+}
+
+ 
